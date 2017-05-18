c4b99edff3fc4b9b39c3732d8eb6bc0c923bf225
alulab14 <alulab14@inf.pucp.edu.pe>
2017-05-18 16:35:44 -0500

    rotation

--- js/index.js
+++ js/index.js
@@ -2,10 +2,12 @@ var body = document.getElementById('container');
 var width = window.getComputedStyle(body).width.split('px')[0] * 1;
 var height = window.getComputedStyle(body).height.split('px')[0] * 1;
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 60, width / height, 0.1, 1000 );
+var camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 1000);
 
 var posZ = 5;
 var angle = 0;
+var quaternion = new THREE.Quaternion();
+var rotationAxis = new THREE.Vector3(0, 1, 0);
 
 scene.background = new THREE.Color(0xffffff);
 
@@ -13,14 +15,14 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize( width, height );
 body.appendChild( renderer.domElement );
 
-var light = new THREE.DirectionalLight( 0xffffff );
-light.position.set( 0, 1, 1 ).normalize();
+var light = new THREE.DirectionalLight(0xffffff);
+light.position.set(0, 1, 1).normalize();
 scene.add(light);
 
 var texture = new THREE.TextureLoader().load('textures/box.jpg');
 
-var geometry = new THREE.BoxGeometry( 1, 1, 1);
-var material = new THREE.MeshPhongMaterial( { map: texture } );
+var geometry = new THREE.BoxGeometry(1, 1, 1);
+var material = new THREE.MeshPhongMaterial({ map: texture });
 var cubes = [];
 
 for (var i = 0; i < 5; i++) {
@@ -30,35 +32,30 @@ for (var i = 0; i < 5; i++) {
   scene.add(cubes[i]);
 }
 
-// camera.position.z = posZ;
-// camera.position.x = 1;
-camera.matrixAutoUpdate = false;
-
 function render() {
-  requestAnimationFrame( render );
-  camera.position.set(1, 0, posZ);
-  camera.rotation.set(0, angle, 0)
-  // var m = new THREE.Matrix4().makeTranslation(1,0,posZ);
-  // camera.matrix.copy(m);
-  camera.updateMatrix();
-  renderer.render( scene, camera );
+  requestAnimationFrame(render);
+  camera.position.set(0, 0, posZ).applyAxisAngle(rotationAxis, angle);
+  camera.rotation.set(0, angle, 0);
+  renderer.render(scene, camera);
 }
 
 document.body.addEventListener('keydown', function(e) {
   switch (e.key) {
     case 'ArrowUp':
       posZ -= 0.1;
-      // camera.position.z -= 0.1;
+      console.log(camera.position);
       break;
     case 'ArrowDown':
       posZ += 0.1;
-      // camera.position.z += 0.1;
+      console.log(camera.position);
       break;
     case 'ArrowRight':
       angle -= 0.1;
+      console.log(camera.position);
       break;
     case 'ArrowLeft':
       angle += 0.1;
+      console.log(camera.position);
       break;
     default:
       break;
